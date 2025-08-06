62789a9f6debd779ba7d2b2f266f7499a0748a2f
Liam C - Lab 101 <1034370@student.sae.edu.au>
2025-08-06 11:17:44 +1000

    week eleven
    abstracts / genereics/ singletons

--- Assets/Scripts/Week Ten/Hologram.cs
+++ Assets/Scripts/Week Ten/Hologram.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Hologram : MonoBehaviour
 {
+    [SerializeField] private int health = 2;
+    [SerializeField] private static float speed = 10.1f;
     private void OnEnable()
     {
         //HologramEventManager.OnSpacebarPressed.AddListener(HideTheCube);
@@ -16,6 +18,24 @@ public class Hologram : MonoBehaviour
         //HologramEventManager.OnSpacebarPressed.RemoveListener(HideTheCube);
         HologramEventManager.onSpacebarPressed -= HideTheCube;
         HologramEventManager.onEPressed -= ShowTheCube;
+    }
+
+    private void Start()
+    {
+        //normal variable
+        print($"Health of {gameObject.name} is {health} Nice!");
+        // static variable 
+        speed = Random.Range(2.5f, 100f);
+        print($"Speed static variable is set to {speed} ! nice !");
+   
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            print($"Speed of {gameObject.name} is {speed} Nice!");
+        }
     }
 
     private void HideTheCube()
